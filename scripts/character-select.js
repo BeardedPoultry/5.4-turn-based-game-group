90e26418a4da070092e6d64f3bdaafacc4a9c33f
Matt Chastain <matthewdchastain@gmail.com>
2015-06-06 15:13:10 -0400

    Click now activates hero, villain, and location generation; popup hide button functional

--- scripts/character-select.js
+++ scripts/character-select.js
@@ -8,7 +8,7 @@ window.GameApp = window.GameApp || {};
 		GameApp.vent.trigger('hero:selected', target);
 	});
 
-	function GenerateHero(id) {
+	GameApp.GenerateHero = function GenerateHero(id) {
 		this.health = (Math.round(Math.random() * 50) + 50);
 		this.attack = function(target) {
 			var damage = (Math.round(Math.random() * 10) + 15);
@@ -16,11 +16,11 @@ window.GameApp = window.GameApp || {};
 			return damage;
 		}
 		this.avatar = images[id]();
-		this.location = images['location' + randomize()]();
+		GameApp.arena = images['arena' + randomize()]();
 		GameApp.villain = new GenerateVillain();
 	}
 
-	function GenerateVillain(id) {
+	function GenerateVillain() {
 		this.health = (Math.round(Math.random() * 50) + 50);
 		this.attack = function(target) {
 			var damage = (Math.round(Math.random() * 10) + 15);
@@ -31,40 +31,45 @@ window.GameApp = window.GameApp || {};
 
 	}
 
+	function healthChange(target, damage) {
+		target.health -= damage;
+		return target.health;
+	}
+
 	function randomize() {
 		return Math.floor(Math.random() * 3) + 1;
 	}
 
 	var images = {
 		'hero1': function () {
-			return '../asset/heros/IMG_0278.PNG';
+			return '../asset/heros/hero1.png';
 		},
 		'hero2': function () {
-			return '../asset/heros/IMG_0278.PNG';
+			return '../asset/heros/hero2.png';
 		},
 		'hero3': function () {
-			return '../asset/heros/IMG_0278.PNG';
+			return '../asset/heros/hero3.png';
 		},
-		'location1': function () {
-			return '../asset/heros/IMG_0278.PNG';
+		'arena1': function () {
+			return '../asset/locations/arena1.jpg';
 		},
-		'location2': function () {
-			return '../asset/heros/IMG_0278.PNG';
+		'arena2': function () {
+			return '../asset/locations/arena2.jpg';
 		},
-		'location3': function () {
-			return '../asset/heros/IMG_0278.PNG';
+		'arena3': function () {
+			return '../asset/locations/arena3.jpg';
 		},
 		'villain1': function () {
-			return '../asset/heros/IMG_0278.PNG';
+			return '../asset/villain/villain1.png';
 		},
 		'villain2': function () {
-			return '../asset/heros/IMG_0278.PNG';
+			return '../asset/villain/villain2.png';
 		},
 		'villain3': function () {
-			return '../asset/heros/IMG_0278.PNG';
+			return '../asset/villain/villain3.png';
 		}
 	}
 
-	GameApp.hero = new GenerateHero('hero1');
+	// GameApp.hero = new GenerateHero('hero1');
 
 })();
--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,9 @@
     // fetchMessages();
     // window.setInterval(fetchMessages, 30000);
 
+    $(document).on('click', '.characterSelectButton', function(e) {
+    	$('.popup-div').hide();
+    });
 
   });
 
